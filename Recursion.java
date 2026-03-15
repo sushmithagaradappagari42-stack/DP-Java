5d186a9f7e1c8e480a4d90694f36081d475511c6
sushmithagaradappagari42-stack <sushmithagaradappagari42@gmail.com>
2026-03-15 14:50:53 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,11 @@
+public class Recursion{
+public static int fact(int n){
+if(n<=1){
+return n;
+}
+return n*fact(n-1);
+}
+public static void main(String args[]){
+System.out.println(fact(8));
+}
+}
